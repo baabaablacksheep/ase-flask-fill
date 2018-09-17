6edda1d1bf3e37e4a7de61efa71dd28ca6c0e4f5
sahan.d <sahan.d@creativesoftware.com>
2018-09-17 21:31:27 +0530

    new MainPanel

--- src/panels/MainPanel.java
+++ src/panels/MainPanel.java
@@ -1,25 +1,26 @@
 package panels;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+        import javax.swing.*;
+        import java.awt.*;
+        import java.awt.event.MouseEvent;
+        import java.awt.event.MouseListener;
 
 public class MainPanel {
 
     private JFrame window= new JFrame("Multiple panels.MainPanel");
     CenterPanel panel_01= new CenterPanel();
     JPanel panel_02=new JPanel();
-    JButton fillButton =new JButton("Fill");
-    JButton drainButton=new JButton("Drain");
+    JButton fillButton =new CustomButton("Fill");
+    JButton drainButton=new CustomButton("Drain");
 
     private static MainPanel panelObj;
 
     private MainPanel(){
-        panel_01.setBackground(Color.CYAN);
+        panel_01.setBackground(Color.LIGHT_GRAY);
         panel_02.setBackground(Color.DARK_GRAY);
 
         panel_02.add(drainButton);
+        panel_02.add(Box.createRigidArea(new Dimension(100,0)));
         panel_02.add(fillButton);
 
         window.add(panel_01,BorderLayout.CENTER);
@@ -68,7 +69,7 @@ public class MainPanel {
             public void mouseExited(MouseEvent e) {}
         });
 
-        window.setSize(500,800);
+        window.setSize(600,800);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
     }
@@ -83,5 +84,51 @@ public class MainPanel {
             }
         }
         return panelObj;
+    }
+
+    /*CustomButton class was created to add customised buttons for Fill and Drain */
+
+    class CustomButton extends JButton implements MouseListener{
+        Font defaultFont = new Font("Tahoma",Font.PLAIN,30);
+        Color textColor = Color.decode("#ffffff");
+        Color backgroundColor = Color.decode("#428bca");
+        Color hoverColor = Color.decode("#5bc0de");
+
+        public CustomButton(String s) {
+            s = s.toUpperCase();
+            this.setFocusPainted(false);
+            this.setText(s);
+            this.setBorder(null);
+            this.setForeground(textColor);
+            this.setBackground(backgroundColor);
+            this.setFont(defaultFont);
+            this.setOpaque(true);
+            this.setPreferredSize(new Dimension(150, 80));
+            addMouseListener(this);
+        }
+
+        public void setBackgroundColor(Color color) {
+            backgroundColor = color;
+        }
+        public void setHoverColor(Color color) {
+            hoverColor = color;
+        }
+
+        @Override public void mouseClicked(MouseEvent me) {}
+        @Override public void mouseReleased(MouseEvent me) {}
+        @Override public void mousePressed(MouseEvent me) {}
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            if (e.getSource()==this) {
+                this.setBackground(this.hoverColor);
+            }
+        }
+        @Override
+        public void mouseExited(MouseEvent e) {
+            if (e.getSource()==this) {
+                this.setBackground(this.backgroundColor);
+            }
+        }
     }
 }
